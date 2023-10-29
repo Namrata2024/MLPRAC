1dbea9d9966b178348a20d7825b6bd86a3bfc2ec
Namrata2024 <76105060+Namrata2024@users.noreply.github.com>
2023-10-29 18:49:47 +0530

    Update exp3.py

--- exp3.py
+++ exp3.py
@@ -1,42 +1,39 @@
-
-
-
 import matplotlib.pyplot as plt
+import math
 
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 5, 4, 5]
 
-x=[1,2,3,4,5]
-y=[2,4,5,4,5]
+x_mean = sum(x) / len(x)
+y_mean = sum(y) / len(y)
 
+numerator = sum((xi - x_mean) * (yi - y_mean) for xi, yi in zip(x, y))
+denominator = sum((xi - x_mean) ** 2 for xi in x)
 
-x_mean=sum(x)/len(x)
-y_mean=sum(y)/len(y)
-
-
-numerator=sum((xi-x_mean)*(yi-y_mean) for xi,yi in zip(x,y))
-denominator=sum((xi-x_mean)**2 for xi in x)
-
-
-m=numerator/denominator
-
-b=y_mean-m*x_mean
+m = numerator / denominator
+b = y_mean - m * x_mean
 
 def predict(x):
-    return m*x+b
+    return m * x + b
 
+# Calculate RMSE
+rmse = math.sqrt(sum((yi - predict(xi)) ** 2 for xi, yi in zip(x, y)) / len(x))
 
-new_x=int(input("Enter the value of x for testing the regression model : "))
+# Print slope, intercept, and RMSE
+print(f'Slope (m): {m}')
+print(f'Intercept (b): {b}')
+print(f'RMSE: {rmse}')
 
-print(f'predicted value of y is: {predict(new_x)}')
+new_x = int(input("Enter the value of x for testing the regression model: "))
 
-plt.scatter(x,y,label='data points')
+print(f'Predicted value of y is: {predict(new_x)}')
 
-regression_line=[predict(val)for val in x]
+plt.scatter(x, y, label='data points')
 
-plt.plot(x,regression_line,color='red',label='regression line')
+regression_line = [predict(val) for val in x]
 
+plt.plot(x, regression_line, color='red', label='regression line')
 plt.xlabel('x')
 plt.ylabel('y')
-
 plt.legend()
 plt.show()
-
